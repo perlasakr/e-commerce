245299d8aaa223eb9b2c7837b8d08838957e3884
Perla Sakr <perlasakr.175@gmail.com>
2021-07-22 21:20:13 +0300

    alert onclick size js

---
+++ js/product.js
@@ -0,0 +1,16 @@
+const value = 10;
+console.log("hello perla");
+console.log("value: " + value);
+
+function logNumber(num) {
+    // const num = 10
+    console.log("my number is: " + num);
+}
+
+function buttonClick() {
+    alert("I am an alert box!");
+}
+
+function sizeClick(size) {
+    alert("alert: " + size);
+}
